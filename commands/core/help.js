99ad25186fb9b5d7073b8c7907994e07b7403469
Maiwald <82387611+Maiwaldgithub@users.noreply.github.com>
2023-01-23 16:32:02 +0100

    Changed footer of help command

--- commands/core/help.js
+++ commands/core/help.js
@@ -18,7 +18,7 @@ module.exports = {
         embed.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
 
         embed.setTimestamp();
-        embed.setFooter('omegalul', message.author.avatarURL({ dynamic: true }));
+        embed.setFooter('MaiBot v4', message.author.avatarURL({ dynamic: true }));
 
         message.channel.send({ embeds: [embed] });
     },
